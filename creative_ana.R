1b46c8ba80eb9510e841c85e30af162074b82533
bbarres <benoit.barres@gmail.com>
2018-10-08 11:57:45 +0200

    updated versions of plot for boscalid and dodine

--- creative_ana.R
+++ creative_ana.R
@@ -11,6 +11,7 @@ library(gdata)
 
 #load the global dataset
 creadat<-read.table("creative_data.txt",header=T,sep="\t")
+collist<-c("forestgreen","black")
 
 
 ###############################################################################
@@ -32,24 +33,29 @@ pdf("boscalid.pdf",width=12,height=30)
 op<-par(mfrow=c(10,4))
 for (i in 1: dim(table(bosc.dat$strain_ID))[1]) {
   datatemp<-bosc.dat[bosc.dat$strain_ID==names(table(bosc.dat$strain_ID))[i],]
+  couleur<-collist[as.numeric(datatemp$strain_type)]
+  typeline<-as.numeric(datatemp$species)[1]
   if (is.na(datatemp[1,"perc_croiss"])==TRUE) {
     tempx<-data.frame("strain_ID"=names(table(bosc.dat$strain_ID))[i],
                       "ED50"=c("NA"))
     REZbos<-rbind(REZbos,tempx)
-    plot(0,1,ylim=c(-10,120),xlim=c(0,30), 
+    plot(0,1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
          main=names(table(bosc.dat$strain_ID))[i])
   } else {
     temp.m1<-drm(perc_croiss~dose,
                  data=datatemp,
-                 fct=LL.4())
-    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),
-         main=names(table(bosc.dat$strain_ID))[i])
+                 fct=LN.4())
+    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         lty=typeline,main=names(table(bosc.dat$strain_ID))[i])
+    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         lty=typeline,type="confidence",add=TRUE)
     temp<-ED(temp.m1,50,type="absolute")
     tempx<-data.frame("strain_ID"=names(table(bosc.dat$strain_ID))[i],
                       "ED50"=temp[1])
     REZbos<-rbind(REZbos,tempx)
   }
 }
+par(op)
 dev.off()
 
 REZbos$ED50[REZbos$ED50>30]<-30
@@ -86,39 +92,30 @@ pdf("dodine.pdf",width=12,height=30)
 op<-par(mfrow=c(10,4))
 for (i in 1: dim(table(dodi.dat$strain_ID))[1]) {
   datatemp<-dodi.dat[dodi.dat$strain_ID==names(table(dodi.dat$strain_ID))[i],]
+  couleur<-collist[as.numeric(datatemp$strain_type)]
+  typeline<-as.numeric(datatemp$species)[1]
   if (is.na(datatemp[1,"perc_croiss"])==TRUE) {
     tempx<-data.frame("strain_ID"=names(table(dodi.dat$strain_ID))[i],
                       "ED50"=c("NA"))
-    REZbos<-rbind(REZbos,tempx)
-    plot(0,1,ylim=c(-10,120),xlim=c(0,30), 
+    REZdod<-rbind(REZdod,tempx)
+    plot(0,1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
          main=names(table(dodi.dat$strain_ID))[i])
   } else {
     temp.m1<-drm(perc_croiss~dose,
                  data=datatemp,
-                 fct=LL.4())
-    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),
-         main=names(table(dodi.dat$strain_ID))[i])
+                 fct=LN.4())
+    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         lty=typeline,main=names(table(dodi.dat$strain_ID))[i])
+    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         lty=typeline,type="confidence",add=TRUE)
     temp<-ED(temp.m1,50,type="absolute")
     tempx<-data.frame("strain_ID"=names(table(dodi.dat$strain_ID))[i],
                       "ED50"=temp[1])
-    REZbos<-rbind(REZbos,tempx)
+    REZdod<-rbind(REZdod,tempx)
   }
 }
+par(op)
 dev.off()
-
-REZbos$ED50[REZbos$ED50>30]<-30
-plot(REZbos$ED50[order(REZbos$ED50)]/0.39,main="Boscalid",xlab="Souches ID",
-     ylab="FR",las=1)
-abline(0.39/0.39,0,col="green4",lwd=2)
-abline(3.9/0.39,0,col="red",lwd=2)
-#export to pdf 10 x 6 inches
-write.table(REZbos,file="REZbos.txt",quote=FALSE,sep="\t",row.names=FALSE)
-
-hist(REZbos$ED50[order(REZbos$ED50)]/0.39,main="Boscalid",xlab="FR Classes",
-     breaks=c(0,10,20,30,40,50,60,70,80,90,100,110,120,130,140,150),
-     las=1,col=heat.colors(8)[8:1],ylim=c(0,450))
-abline(v=10,col="red",lwd=3)
-#export to pdf 4.5 x 9 inches
 
 
 
