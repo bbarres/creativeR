327c6ec8bb66dc24de0737cf224488a49e4a46e5
bbarres <benoit.barres@gmail.com>
2019-06-18 23:56:01 +0200

    rewriting the code for more flexibility with different type of tests and species

---
+++ creative_VenturiaMyc.R
@@ -0,0 +1,60 @@
+###############################################################################
+#Analysis for Venturia by mycelial growth bioassay
+###############################################################################
+
+#subsetting the global dataset
+ventuMyc.dat<-creadat[creadat$species=="V. inaequalis" | 
+                        creadat$species=="V. asperata",]
+ventuMyc.dat<-ventuMyc.dat[ventuMyc.dat$test_type=="mycelial_growth",]
+ventuMyc.dat$species<-factor(ventuMyc.dat$species,
+                             levels=rev(levels(ventuMyc.dat$species)))
+ventuMyc.dat<-drop.levels(ventuMyc.dat,reorder=FALSE)
+ventuMyc.dat<-ventuMyc.dat[!is.na(ventuMyc.dat$perc_croiss),]
+
+collist<-c("forestgreen","black")
+
+
+####boscalild
+subdat<-ventuMyc.dat[ventuMyc.dat$active_substance=="trifloxystrobine",]
+#some individual never reach an inhibition of 50%, event for the highest 
+#tested concentration. 
+subdat_rez<-as.character(subdat[subdat$dose=="30" & subdat$perc_croiss>50,
+                                "strain_ID"])
+ifelse(length(subdat_rez)==0,
+       REZsub<-data.frame("strain_ID"=as.character(),"ED50"=as.character()),
+       REZsub<-data.frame("strain_ID"=subdat_rez,
+                          "ED50"=paste(">",max(subdat$dose),sep="")))
+#we limit the dataset to the sample that reach somehow a IC of 50%
+subdat<-subdat[!(subdat$strain_ID %in% subdat_rez),]
+
+subdat<-drop.levels(subdat,reorder=FALSE)
+pdf(file="output/VeMyboscalid.pdf",width=12,height=30)
+op<-par(mfrow=c(10,4))
+for (i in 1: dim(table(subdat$strain_ID))[1]) {
+  datatemp<-subdat[subdat$strain_ID==names(table(subdat$strain_ID))[i],]
+  couleur<-collist[as.numeric(datatemp$strain_type)]
+  typeline<-as.numeric(datatemp$species)[1]
+  if (is.na(datatemp[1,"perc_croiss"])==TRUE) {
+    tempx<-data.frame("strain_ID"=names(table(subdat$strain_ID))[i],
+                      "ED50"=c("NA"))
+    REZsub<-rbind(REZsub,tempx)
+    plot(0,1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         main=names(table(subdat$strain_ID))[i])
+  } else {
+    temp.m1<-drm(perc_croiss~dose,
+                 data=datatemp,
+                 fct=LL.4())
+    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         lty=typeline,main=names(table(subdat$strain_ID))[i])
+    plot(temp.m1,ylim=c(-10,120),xlim=c(0,30),col=couleur,
+         lty=typeline,type="confidence",add=TRUE)
+    temp<-ED(temp.m1,50,type="absolute")
+    tempx<-data.frame("strain_ID"=names(table(subdat$strain_ID))[i],
+                      "ED50"=as.character(temp[1]))
+    REZsub<-rbind(REZsub,tempx)
+  }
+}
+par(op)
+dev.off()
+
+REZ_VeMyBosc<-REZsub
